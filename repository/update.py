da5a060bf0053aac1921769644c35bd2570f6d6e
Ian Mayo <ian@planetmayo.com>
2019-10-19 12:59:44 +0100

    checking for zip file

--- repository/update.py
+++ repository/update.py
@@ -2,7 +2,11 @@ import datetime
 import os
 import xml.etree.cElementTree as ET
 
+# see if this filename is in the array of filenames
 def checkPresent(arr, fileName, curResult):
+    # don't bother testing if we've already failed
+    if curResult is False:
+        return False
     if fileName not in arr:
         print (fileName + " is missing")
         return False
@@ -13,11 +17,18 @@ def checkFolder():
     # do some checks on this folder
     arr = os.listdir()
 
+    # initialise success flag
     result = True
 
     result = checkPresent(arr, "compositeContent.xml", result)
     result = checkPresent(arr, "compositeArtifacts.xml", result)
     result = checkPresent(arr, "p2.index", result)
+
+    if result is True:
+        zipFound = findZipFile()
+        if zipFound is None:
+            print("Zip-file not found in folder")
+            result = False
 
     return result
 
@@ -25,12 +36,14 @@ def getDTG():
     now = datetime.datetime.now()
     return now.strftime("%Y_%m_%d__%H_%M_%S")
 
+# see if there is a zip=file in the current folder
 def findZipFile():
     arr = os.listdir()
     for file in arr:
         if(file.endswith(".zip")):
             return file
 
+# update the children element
 def updateChildren(contentFile, newFile):
     tree = ET.parse(contentFile)
     root = tree.getroot() 
